b40e5ce1dc1a5bf2fdda18f244d4a2ebe45b3500
lucas labadens <lucaslabadens@openroam-prg-sg-1-172-160.net.univ-paris-diderot.fr>
2016-04-07 10:15:54 +0200

    ServeurTCP

---
+++ ServeurTcp.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+
+
+public class ServeurTcp{
+    Entity ent;
+    public ServeurTcp(Entity e){
+	this.ent = e;
+    } 
+    public void runServ(){
+	try{
+	    int port_tcp = Integer.parseInt(ent.port_tcp);
+	    ServerSocket server = new ServerSocket(port_tcp);
+	    while(true){
+		Socket socket=server.accept();
+		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter pw=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+		String text = "WELC "+ent.ip_next+" "+ent.port_udp_next+" "+ent.ip_diff+" "+ent.port_diff;
+		pw.print(text);
+		pw.flush();
+		text=br.readLine();
+		String[] token =text.split(" ");
+		if(token[0].equals("WELC")){
+		    ent.ip_next = token[1];
+		    ent.port_udp_next = token[2];
+		}
+	    }
+	}
+	catch(Exception e){
+	    System.out.println(e);
+	    e.printStackTrace();
+	}
+    }
+}
