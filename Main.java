a127c6aeabb96174873a11e1e950dea0f5d444e6
ahumbe42 <abel.humbert@laposte.net>
2016-04-07 10:22:18 +0200

    ok

--- Main.java
+++ Main.java
@@ -1,52 +1,97 @@
 import java.util.*;
 import java.net.InetAddress; 
+import java.net.UnknownHostException;
+
 
 public class Main{
 
     public static void main (String[] args){
 	Scanner scanner = new Scanner (System.in);
-	System.out.println("what's your name ?");
-	String id = scanner.nextLine();
-	System.out.println("port TCP ?");
-	String port_tcp = scanner.nextLine();
-	System.out.println("port UDP ?");
-	String port_udp = scanner.nextLine();
-	InetAddress IA = InetAddress.getLocalHost(); 
-	String ip = IA.getHostAddress();
-	String cmd = scanner.nextLine();
-	String[] arr = cmd.split(" ");
-	Entity me;
-	switch (arr[0]){
-	case "connect":
-	    me = new Entity(id, port_udp, port_tcp);
-	    ClientTcp cl=new ClientTcp(me, port_udp, ip);
-	    int a=cl.clientTCP("connect");
-	    if(a==0){
-		//serveur tcp
-		//serveur udp
-		boolean quit = true;
-		while(quit){
-		    String cm = scanner.nextLine();
-
+	boolean arg = true;
+	while(arg){
+	    System.out.println("what's your name ?");
+	    String id = scanner.nextLine();
+	    System.out.println("port TCP ?");
+	    String port_tcp = scanner.nextLine();
+	    System.out.println("port UDP ?");
+	    String port_udp = scanner.nextLine();
+	    String ip;
+	    try{
+		InetAddress IA = InetAddress.getLocalHost(); 
+		ip = IA.getHostAddress();
+		Entity me;
+		ClientTcp cl;
+		int a;
+		boolean is_connected=false;
+		boolean b=true;
+		while(b){
+		    String cmd = scanner.nextLine();
+		    String[] arr = cmd.split(" ");
+		    switch (arr[0]){
+		    case "connect":
+			me = new Entity(id, port_udp, port_tcp);
+			cl=new ClientTcp(me, port_udp, ip);
+			a=cl.clientTCP("connect");
+			if(a==0){
+			    //serveur tcp
+			    //serveur udp
+			    is_connected=true;
+			}else{
+			    System.out.println("wrong arguments");
+			    System.out.println();
+			    b=false;
+			}
+			break;
+		    case "create":
+			System.out.println("multi diff adress ?");
+			String multi_diff = scanner.nextLine();
+			System.out.println("multi diff port ?");
+			String port_diff = scanner.nextLine();
+			me = new Entity(id, port_udp, port_tcp, multi_diff, port_diff);
+			//serveur udp
+			//serveur tcp
+			//tests : wrong args b=false
+			is_connected=true;
+			break;
+		    case "dupl":
+			me = new Entity(id, port_udp, port_tcp);
+			cl=new ClientTcp(me, port_udp, ip);
+			a=cl.clientTCP("dupl");
+			if(a==0){
+			    //serveur tcp
+			    //serveur udp
+			    is_connected=true;
+			}else{
+			    System.out.println("wrong arguments");
+			    System.out.println();
+			    b=false;
+			}
+			break;
+		    case "who's" :
+			if(is_connected=true){
+			    //blabla
+			}else{
+			    System.out.println("use connect, create or dupl");
+			}
+			break;
+		    case "quit ring" :
+			is_connected=false;
+			me=null;
+			//serveurs=null
+			break;
+		    case "quit all" : 
+			b=false;
+			arg=false;
+			break;
+		    default : 
+			System.out.println("incompatible command");
+		    }
 		}
+	     }
+	    catch(UnknownHostException e){
+		System.out.println(e);
+		e.printStackTrace();
 	    }
-	    break;
-	case "create":
-	    System.out.println("multi diff adress ?");
-	    String multi_diff = scanner.nextLine();
-	    System.out.println("multi diff port ?");
-	    String port_diff = scanner.nextLine();
-	    me = new Entity(id, port_udp, port_tcp, multi_diff, port_diff);
-	    //serveur udp
-	    //serveur tcp
-	    break;
-	case "dupl":
-	    me = new Entity(id, port_udp, port_tcp);
-	    
-	    break;
-	default : 
-	    System.out.println("incompatible command");
 	}
     }
 }
-
