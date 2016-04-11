0c8270f1f89de556555c61b720a98aa502c33fd7
lucas labadens <lucaslabadens@openroam-prg-sg-1-173-1.net.univ-paris-diderot.fr>
2016-04-11 11:55:23 +0200

    lulu

--- Mess.java
+++ Mess.java
@@ -1,9 +1,9 @@
-#define M_SIZE_MAX 512        /***< maximum size for a message (octets) */
+//#define M_SIZE_MAX 512        /***< maximum size for a message (octets) */
 
 /**
  * A structure to represent a message
  */
-typedef struct{
+/*typedef struct{
   char idm [8];
   char mess [M_SIZE_MAX];
 } mess;
@@ -18,3 +18,4 @@ public class Mess{
     }
     
 }
+*/
