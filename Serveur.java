b3ffaa9c4031abcffafc198418a61a85549214d2
lucas labadens <lucaslabadens@MacBook-Pro-de-lucas.local>
2016-04-26 13:44:18 +0200

    Serveur

---
+++ Serveur.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public abstract class Serveur{
+    Entity ent;
+    public Serveur(Entity e){
+	this.ent = e;
+    }
+    public abstract void runServ(boolean run);
+}
