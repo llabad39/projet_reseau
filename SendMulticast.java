8c49d00498d19ec507aad65e5a0cdcbf493f9204
lucas labadens <lucaslabadens@MacBook-Pro-de-lucas.local>
2016-04-26 18:17:40 +0200

    j

--- SendMulticast.java
+++ SendMulticast.java
@@ -8,17 +8,23 @@ public class SendMulticast{
     }
 
     public void send(String text){
-	DatagramSocket dso=new DatagramSocket();
-	byte[] buf = text.getBytes();
-	InetSocketAddress ia=new InetSocketAddress(this.ent.ip_diff,Integer.parseInt(this.ent.port_diff));
-	/*	if(this.ent.ip_next2 != null){
-	    int p2 = Integer.parseInt(this.ent.port_diff2);
-	    InetSocketAdress ia2=new InetSocketAddress(this.ent.ip_diff2,p2);
+	try{
+	    DatagramSocket dso=new DatagramSocket();
+	    byte[] buf = text.getBytes();
+	    InetSocketAddress ia=new InetSocketAddress(this.ent.ip_diff,Integer.parseInt(this.ent.port_diff));
+	    if(this.ent.ip_next2 != null){
+		int p2 = Integer.parseInt(this.ent.port_diff2);
+		//DatagramSocket dso2=new DatagramSocket();
+		InetSocketAddress ia2=new InetSocketAddress(this.ent.ip_diff2,p2);
 	    DatagramPacket paquet2=new DatagramPacket(buf,buf.length,ia2);
 	    dso.send(paquet2);
+	    }
+	    
+	    DatagramPacket paquet=new DatagramPacket(buf,buf.length,ia);
+	    dso.send(paquet);
 	}
-	*/
-	DatagramPacket paquet=new DatagramPacket(buf,buf.length,ia);
-	dso.send(paquet);
+	catch(Exception e){
+	    e.printStackTrace();
+	}
     }
 }
